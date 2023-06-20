3190c97a06b714d7236176d5f7f4d73128a67e3d
mdrakibulislam <rakibulislamborkan@gmail.com>
2023-06-21 03:59:03 +0600

    finished

--- src/Components/ContactSection/ContactSection.jsx
+++ src/Components/ContactSection/ContactSection.jsx
@@ -1,9 +1,33 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { useRef } from 'react';
 AOS.init();
-
+import emailjs from '@emailjs/browser';
+import toast, { Toaster } from 'react-hot-toast';
 
 const ContactSection = () => {
+
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_xyc3h5s', 'template_eqj535f', form.current, 'ONEJMwy6NT6X3Qs8C')
+            .then((result) => {
+                toast.success('Your message has been sent, I will response as soon as possible')
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+                toast.error(error.text)
+            });
+
+        e.target.reset()
+    };
+
+
+
+
     return (
         <div data-aos="fade-up" className='main-container pt-20 pb-12 flex flex-col gap-8'>
 
@@ -28,13 +52,18 @@ const ContactSection = () => {
                     </div>
                 </div>
                 <div className='flex flex-col gap-4'>
-                    <h1 className='font-moulpali text-4xl text-white'>👋 Say Hi!</h1>
-                    <textarea className='outline-none w-full p-4 rounded-2xl bg-[#5a5a5a] text-white' placeholder='Enter your message here...' name="" id="" cols="30" rows="7"></textarea>
-                    <button className='bg-mantel text-xl px-6 py-2 rounded-full font-moulpali text-white hover:shadow-xl max-w-fit'>Send Message</button>
+                    <h1 className='font-moulpali text-4xl text-white'>Send me message directly from here</h1>
+                    <form ref={form} onSubmit={sendEmail} action="" className='flex flex-col gap-4'>
+                        <input className='outline-none w-full p-4 rounded-2xl bg-[#5a5a5a] text-white' type="text" name="user_name" id="" placeholder='Please enter your name here' />
+                        <input className='outline-none w-full p-4 rounded-2xl bg-[#5a5a5a] text-white' type="email" name="user_email" id="" placeholder='Please enter your email here' />
+                        <input className='outline-none w-full p-4 rounded-2xl bg-[#5a5a5a] text-white' type="text" name="subject" id="" placeholder='Please enter subjet here' />
+                        <textarea className='outline-none w-full p-4 rounded-2xl bg-[#5a5a5a] text-white' placeholder='Enter your message here...' name="message" id="" cols="30" rows="3"></textarea>
+                        <button type='submit' className='bg-mantel text-xl px-6 py-2 rounded-full font-moulpali text-white hover:shadow-xl max-w-fit'>Send Message</button>
+                    </form>
                 </div>
             </div>
 
-
+            <Toaster />
         </div>
     );
 };
